59320d6883de7aade376fc391651142cb926e577
yaowenqiang <yaowenqiang111@163.com>
2026-07-08 23:20:24 +0800

    add more notes

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,5 @@
 > uvicorn books:app --reload
 
 > uvicorn books:app reload
+
+> xh localhost:8000/books/book_one 
--- main.py
+++ main.py
@@ -28,6 +28,11 @@ BOOKS = [
         'author':'author five',
         'category':'math',
     },
+    {
+        'title':'titleSix',
+        'author':'author six',
+        'category':'math',
+    },
 ]
 
 
@@ -40,5 +45,17 @@ async def first_api():
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+@app.get('/books/{dynamic_param}')
+async def read_all_books(dynamic_param):
+    return {'dynamic_param': dynamic_param}
+
+
 
 
