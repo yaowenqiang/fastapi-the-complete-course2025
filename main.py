126b91bf397b16cf91ee70985cd06fb08ab90f0a
yaowenqiang <yaowenqiang111@163.com>
2026-07-09 23:06:23 +0800

    add more examples

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ BOOKS = [
     },
     {
         'title':'titleSix',
-        'author':'author six',
+        'author':'authorsix',
         'category':'math',
     },
 ]
@@ -51,24 +51,25 @@ async def read_all_books():
 async def read_my_books():
     return {'book_title': 'my favorite book!'}
 
-@app.get('/books/{book_title}')
-async def read_book(book_title: str):
-    for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
-            return book
+#@app.get('/books/{book_title}')
+#async def read_book(book_title: str):
+#    for book in BOOKS:
+#        if book.get('title').casefold() == book_title.casefold():
+#            return book
 
-@app.get('/books/{dynamic_param}')
-async def read_all_books(dynamic_param):
-    return {'dynamic_param': dynamic_param}
+#@app.get('/books/{dynamic_param}')
+#async def read_all_books(dynamic_param):
+#    return {'dynamic_param': dynamic_param}
 
 
 @app.get('/books/{book_author}')
 async def read_category_by_query(book_author: str,category: str):
-    print(category)
     print(book_author)
+    print(category)
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and book.get('catetory').casefold() == category.casefold():
+        print(book.get('author')) 
+        if (book.get('author').casefold() == book_author.casefold()) and (book.get('catetory').casefold() == category.casefold()):
             books_to_return.append(book)
     return books_to_return
 
