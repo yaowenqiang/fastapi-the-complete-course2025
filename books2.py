f75d8e3c96c81d4731b7c03d3a64738ce8a6e548
yaowenqiang <yaowenqiang111@163.com>
2026-07-20 23:51:03 +0800

    add pydantic demo

--- books2.py
+++ books2.py
@@ -1,5 +1,9 @@
+from email.quoprimime import body_check
+from pydantic import BaseModel
+
 import uvicorn
 from fastapi import FastAPI, Body
+from pygments.lexers import func
 
 app = FastAPI()
 
@@ -17,6 +21,12 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
 
 
 BOOKS = [
@@ -33,6 +43,17 @@ BOOKS = [
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+# @app.post('/create-book')
+# async def create_book(book_request=Body()):
+#     BOOKS.append(book_request)
+
+@app.post('/create-book')
+async def create_book(book_request=BookRequest):
+    new_book = Book(**book_request.dict())
+    # new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+
 
 if __name__ == "__main__":
     uvicorn.run('books2:app', host="0.0.0.0", port=8001, reload=True)
