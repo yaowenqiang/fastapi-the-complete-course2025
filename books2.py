ed5be2d09aee35d511d245be41eda65e1b84d66a
yaowenqiang <yaowenqiang111@163.com>
2026-07-23 22:47:09 +0800

    add swagger pydantic configuration demo

--- books2.py
+++ books2.py
@@ -25,11 +25,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
-    rating: int = Field(gt=-1, lt=5)
+    rating: int = Field(gt=-1, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title":"A new book",
+                "author": "jack",
+                "description": "A new description of a book",
+                "rating":5
+            }
+        }
+    }
 
 def find_book_id(book: Book):
     # if len(BOOKS) > 0:
