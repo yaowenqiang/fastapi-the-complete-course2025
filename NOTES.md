7c24bdc966006a78c4243929af095f4fe74b841d
yaowenqiang <yaowenqiang111@163.com>
2026-07-30 23:48:38 +0800

    add db demo

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,13 @@
 > %20 = space in a URL
 
 > pip install sqlalchemy
+> 
+> sqlite> .mode column
+  
+> sqlite> .mode markdown
+  
+> sqlite> .mode box
+  
+> sqlite> .mode table
+
+> sqlite3 will reuse the primary key if record is deleted
--- todo/main.py
+++ todo/main.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI
+from typing import Annotated
+import uvicorn
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 import models
+from models import Todos
 
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get('/')
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8003, reload=True)
