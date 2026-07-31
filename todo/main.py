49a1b65ccd428a12e8a385448d4d4d83afa4bb9b
yaowenqiang <yaowenqiang111@163.com>
2026-07-31 21:52:10 +0800

    add fetch one todo demo

--- todo/main.py
+++ todo/main.py
@@ -1,7 +1,10 @@
+from operator import gt
 from typing import Annotated
+
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 from models import Todos
@@ -22,9 +25,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+@app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int=Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo Not Found")
 
 
 if __name__ == "__main__":
